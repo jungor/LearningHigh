a57a1501d2c5d31ac7f21a71a3b888468f3852a7
jungor <287150625@qq.com>
2016-07-08 00:46:50 +0800

    send index file at entry point '/'

--- config/express.js
+++ config/express.js
@@ -10,6 +10,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var compress = require('compression');
 var methodOverride = require('method-override');
+var path = require('path')
 
 
 
@@ -50,7 +51,8 @@ module.exports = function(app, config) {
   app.use(methodOverride());
 
   app.get('/', (req, res)=>{
-    res.send('hello world')
+    res.sendFile(path.join(__dirname,
+    '../app_angular/index.html'))
   })
 
   var controllers = glob.sync(config.root + '/app/controllers/*.js');
