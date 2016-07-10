9a14c2a0d6f0139d3fb147878016cc8229de4d85
lmoavne <642148322@qq.com>
2016-07-10 11:38:59 +0800

    init font-end home

---
+++ public/app_angular/js/app.js
@@ -0,0 +1,24 @@
+/**
+* qingYun Module
+*
+* Description
+*/
+var qingYun = angular.module('qingYun', ['ui.router']);
+
+qingYun.run(function($rootScope, $state, $stateParams){
+	$rootScope.$state = $state;
+	$rootScope.$stateParams = $stateParams;
+});
+
+qingYun.config(function($stateProvider, $urlRouterProvider) {
+	$urlRouterProvider.otherwise("/index");
+	$stateProvider
+		.state('index', {
+			url: '/index',
+			views: {
+				'':{
+					templateUrl: "/app_angular/templates/home.html"
+				}
+			}
+		});
+});
