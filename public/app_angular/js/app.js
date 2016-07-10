aab40f9d4b463eefd7fba050f45c72dc6c24af70
RqwithQw <429857668@qq.com>
2016-07-10 15:32:55 +0800

    xrq v1

--- public/app_angular/js/app.js
+++ public/app_angular/js/app.js
@@ -3,22 +3,22 @@
 *
 * Description
 */
-var qingYun = angular.module('qingYun', ['ui.router']);
+var qingYun = angular.module('qingYun', ['ui.router', 'home']);
 
 qingYun.run(function($rootScope, $state, $stateParams){
-	$rootScope.$state = $state;
-	$rootScope.$stateParams = $stateParams;
+    $rootScope.$state = $state;
+    $rootScope.$stateParams = $stateParams;
 });
 
 qingYun.config(function($stateProvider, $urlRouterProvider) {
-	$urlRouterProvider.otherwise("/index");
-	$stateProvider
-		.state('index', {
-			url: '/index',
-			views: {
-				'':{
-					templateUrl: "/app_angular/templates/home.html"
-				}
-			}
-		});
+    $urlRouterProvider.otherwise("/index");
+    $stateProvider
+        .state('index', {
+            url: '/index',
+            views: {
+                '':{
+                    templateUrl: "/app_angular/templates/home.html"
+                }
+            }
+        });
 });
