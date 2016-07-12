1755c5b5708adb0407745aad9183fd3380927d2e
lmoavne <642148322@qq.com>
2016-07-12 18:08:12 +0800

    fix event broadcast bug

--- public/app_angular/js/detail.js
+++ public/app_angular/js/detail.js
@@ -17,67 +17,85 @@ detail.config(function($stateProvider, $urlRouterProvider) {
                     templateUrl: '/app_angular/templates/navigation-bar.html'
                 },
                 'pdf@detail': {
-                    templateUrl: '/app_angular/templates/pdf.html',
-                    controller: function($scope) {
-
-                        //console.log(angular.element("#fuck").val());
-                        // console.log($("#myIframe").contents().find("#pageNumber").val());
-                        // $("#myIframe").contents().find("#pageNumber").bind('input propertychange', function($scope) {
-                        //     console.log('change');
-                        // });
-                        // console.log("fuck");
-                        // while($("#myIframe").contents().find("#pageNumber").val() == undefined) {
-
-                        // }
-
-                       
-
-                        var pageNumber = null;
-                        var tid = setInterval(function() {
-                        	var content = document.getElementById("myIframe").contentWindow;
-                            if (pageNumber) {
-                            	
-                            	//  content.document.getElementById("pageNumber").onpropertychange = function() {
-                            	// 	console.log("changed");
-                            	// };
-                            	// document.querySelector("#myIframe #pageNumber").addEventListener('change', function() {
-                            	// 	console.log("changed");
-                            	// });
-                                // $("#myIframe").contents().find("#pageNumber").on('clcik', )
-                                var temp = content.document.getElementById("pageNumber").value;
-                                if (temp != $scope.myPageNumber) {
-                                	// $scope.myPageNumber = temp;
-                                	// console.log('success');
-                                	// console.log($scope.myPageNumber);
-                                	document.getElementById("fuck").value = temp;
-                                	console.log(document.getElementById("fuck").value);
-                                }
-  
-                                
-                                
-                            } else {
-                            	
-                            	pageNumber = content.document.getElementById("pageNumber").value;
-                            	// pageNumber = document.querySelector("#myIframe > #pageNumber").value;
-                                // pageNumber = $("#myIframe").contents().find("#pageNumber").val();
-                                console.log('fdsf')
-                            }
-
-                        }, 1000);
-
-                        
-                        // document.getElementById('myIframe').onload(function() {
-                        // 	// body...
-                        // 	console.log($("#myIframe").contents().find("#pageNumber").val());
-                        // })
-
-
-
-                    }
+                    templateUrl: '/app_angular/templates/pdf.html'
                 },
-                'discuss@detail': {
-                    templateUrl: '/app_angular/templates/discuss.html'
+                'questions@detail': {
+                    templateUrl: '/app_angular/templates/questions.html'
                 }
             }
         });
 })
+
+
+detail.controller('detail-content', ['$scope', '$rootScope', function($scope, $rootScope) {
+    $scope.name = 'detail-content';
+    $rootScope.pageNumber = null;
+    $rootScope.questionId = null;
+
+    $scope.getQA = function() {
+        // get question answers comments
+
+        $scope.$broadcast('to-pdf-quesions', '666');
+        console.log('get question answers comments');
+    }
+}]);
+
+
+detail.controller('pdf', ['$scope', '$rootScope', function($scope, $rootScope) {
+    $scope.name = 'pdf';
+    $scope.myHide = false;
+    $scope.$on('to-pdf-quesions', function(event, data) {
+        console.log(data);
+        console.log(event);
+        $scope.myHide = true;
+    })
+
+}]);
+
+
+detail.controller('questions', ['$scope', '$rootScope', '$interval', function($scope, $rootScope, $interval) {
+    $scope.questionsList = {};
+    $scope.myHide = false;
+    $scope.$on('to-pdf-quesions', function(event, data) {
+        console.log(data);
+        console.log(event);
+        $scope.myHide = true;
+    })
+    
+    var pageNumber = null;
+    $interval(function() {
+        var content = document.getElementById("myIframe").contentWindow;
+        if (pageNumber) {
+            var temp = content.document.getElementById("pageNumber").value;
+            if ($rootScope.pageNumber === null) {
+                //get current page questions
+                $scope.questionsList = {'1':1};
+
+
+                console.log('test 1');
+                $rootScope.pageNumber = temp;
+            }
+            if (pageNumber != temp) {
+                //get current page questions
+                $scope.questionsList = {'1':1, '2':2};
+
+                console.log('test 2');
+                $rootScope.pageNumber = temp;
+                pageNumber = temp;
+            }
+        } else {
+            pageNumber = content.document.getElementById("pageNumber").value;
+        }
+
+    }, 1000);
+
+}]);
+
+
+detail.controller('one-question', ['$scope', function($scope) {
+    $scope.name = "one-question";
+    $scope.show = function() {
+        console.log($scope.tinymceModel);
+    };
+
+}]);
